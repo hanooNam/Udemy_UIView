968f321a22e451f21284f44e2244d2b052fb6038
hanooNam <jnam920329@gmail.com>
2017-04-17 21:53:37 -0400

    end

--- UIScrollView/UIScrollView/ViewController.swift
+++ UIScrollView/UIScrollView/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var images = [UIImageView]()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        for x in 0...2{
+            let image = UIImageView.
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
