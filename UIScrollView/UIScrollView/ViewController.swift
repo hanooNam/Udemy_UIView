d6f74be727e95c937466bd31f8a17e5df4275379
hanooNam <jnam920329@gmail.com>
2017-04-22 02:01:14 -0400

    UIScrollView done

--- UIScrollView/UIScrollView/ViewController.swift
+++ UIScrollView/UIScrollView/ViewController.swift
@@ -12,13 +12,37 @@ class ViewController: UIViewController {
 
     var images = [UIImageView]()
     
+    @IBOutlet weak var scrollView: UIScrollView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+           }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth : CGFloat = 0.0
+        
         for x in 0...2{
-            let image = UIImageView.
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX : CGFloat = 0.0
+            
+            newX = scrollView.frame.size.width / 2 + scrollView.frame.size.width * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: 0, y: scrollView.frame.midY-75, width: 150, height: 150)
+            // 여기의 위치는 uiscrollview에서 지정됨 , subview안에서의 위치 지정이니까
         }
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        
+
     }
 }
 
